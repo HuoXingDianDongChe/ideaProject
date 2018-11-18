07a6fc4f749fe8b2c58bcc058c298356c467e2c3
admin <loadings168@163.com>
2018-11-18 18:24:13 +0800

    第一次修改

--- gittest/src/main/java/git/TestGit1.java
+++ gittest/src/main/java/git/TestGit1.java
@@ -5,5 +5,8 @@ public class TestGit1 {
         System.out.println("1111");
         System.out.println("2222");
         System.out.println("3333");
+
+        System.out.println("5555");
+        System.out.println("6666");
     }
 }

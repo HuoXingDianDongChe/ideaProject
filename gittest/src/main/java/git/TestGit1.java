e7eb8d6c9470e304bb0ddda580bf97a0c633bdbe
admin <loadings168@163.com>
2018-11-18 17:09:52 +0800

    上传git测试案例到本地仓库

---
+++ gittest/src/main/java/git/TestGit1.java
@@ -0,0 +1,9 @@
+package git;
+
+public class TestGit1 {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("2222");
+        System.out.println("3333");
+    }
+}
